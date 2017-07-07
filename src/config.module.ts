7b0badcbd1a7e4ab054d36ad5907fb7ad1f361fe
Joel Trousset <external.z08jtrou@decathlon.com>
2017-07-07 17:51:22 +0200

    feat: better config file + interactive config setup

--- src/config.module.ts
+++ src/config.module.ts
@@ -1,8 +1,12 @@
-const readConfig = require('read-config');
-const os = require('os');
-const path = require('path');
-const fs = require('fs');
+
+import * as os from 'os';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as ini from 'ini';
+import * as _ from "lodash";
 const screenlog = require('./utils/screen.module');
+const readline = require('readline');
+import * as Q from 'q';
 
 class Config {
 
@@ -11,26 +15,82 @@ class Config {
     private loaded: boolean = false;
     constructor() {
 
-        this.configPath = path.join(os.homedir(), '.atgrc.json');
-
+        this.configPath = this.getConfigPath();
         let exists: boolean = fs.existsSync(this.configPath);
-
         if (exists) {
             try {
-                this.config = readConfig(this.configPath);
+                let content: string = fs.readFileSync(this.configPath, 'UTF-8');
+
+                this.config = ini.parse(content);
+
+
                 this.loaded = true;
+
+                // this.config = readConfig(this.configPath);
+                // this.loaded = true;
             } catch (e) {
                 this.loaded = false;
             }
         } else {
             this.loaded = false;
         }
-
-
         if (!this.loaded) {
-            screenlog.error('Cannot read config file %s', this.configPath);
+            this.config = {};
+        }
+     //   this.checkConfigRequirement();
+
+
+
+    }
+
+    private getConfigPath(): string {
+        return path.join(os.homedir(), '.atgconfig');
+
+    }
+
+    private checkRequirements(): Q.Promise<void> {
+
+        var args = ["dynamoHome", "editor"];
+
+        var result :Q.Promise<void>= Q();
+        let self = this;
+        args.forEach(function(t) {
+            result = result.then(
+                ()=>self.checkConfig(t)
+                )
+        });
+        return result;
+        
+        
+    }
+
+    public checkConfig(path: string):Q.Promise<void> {
+
+        let deferred: Q.Deferred<void> = Q.defer<void>();
+
+        if (_.isEmpty(this.get(path))) {
+            screenlog.error('%s is not configured', path);
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+            rl.question('Please set value for ' + path+ '\n', (answer) => {
+                // TODO: Log the answer in a database
+                this.config[path] = ini.safe(answer);
+
+                rl.close();
+                this.save();
+                deferred.resolve();
+            });
+        }else{
+            deferred.resolve();
         }
 
+        return deferred.promise;
+    }
+
+    public save() {
+        fs.writeFileSync(this.configPath, ini.stringify(this.config));
     }
 
     public get(key: string): any {
